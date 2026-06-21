1c0a7d361cbc2cf47464ef3c2695255eb8081cbd
Nirupam <joardernirupam@gmail.com>
2026-06-21 16:46:31 +0530

    Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import {
 } from "recharts";
 
 import "./App.css";
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://smart-traffic-analytics-platform.onrender.com";
 
 
 function App() {
